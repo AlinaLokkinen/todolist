85fce65475a2fe9cda3a0073d0f9fd445450f06d
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-03-07 14:56:10 +0200

    grid lisätty

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
-import TodoTable from "./TodoTable";
+// import TodoTable from "./TodoTable";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 function TodoList() {
 
-    const [task, setTask] = useState({description: "", date: ""});
+    const [task, setTask] = useState({desc: "", priority:"", date: ""});
     const [todos, setTodos] = useState([]);
+
+    const [columnDefs, setColumnDefs] = useState([
+      {field: 'desc', sortable: true, filter: true, floatingFilter: true},
+      { field: "priority", sortable: true, filter: true, 
+    cellStyle: params => params.value === "high" ? {color: 'red'} : {color: 'black'} },
+      {field: 'date', filter: true, floatingFilter: true}
+    ]);
 
     const handleChange = (event) => {
      setTask({...task, [event.target.name]: event.target.value});
@@ -15,18 +26,28 @@ function TodoList() {
        setTodos([...todos, task]);
     };
 
-    function handleDelete(index) {
-      const newTodos = todos.filter((todo, i) => i !== index);
-      setTodos(newTodos);
-    };
+    // function handleDelete(index) {
+    //   const newTodos = todos.filter((todo, i) => i !== index);
+    //   setTodos(newTodos);
+    // }
 
     return (
         <>
         <p>Add todo: </p>
-          <p>Description: <input type="text" onChange={handleChange} value={task.description} name="description" /></p>
+          <p>Description: <input type="text" onChange={handleChange} value={task.desc} name="desc" /></p>
+
+          <p>Priority: <input type="text" onChange={handleChange} value={task.priority} name="priority" /></p>
+
           <p>Date: <input type="text" onChange={handleChange} value={task.date} name="date" /></p>
+
           <button onClick={addTodo}>Add</button>
-          <TodoTable todos={todos} handleDelete={handleDelete} />
+
+          <div className="ag-theme-material" style={{width: 700, height: 500}}>
+            <AgGridReact 
+              rowData={todos}
+              columnDefs={columnDefs}
+            />
+        </div>
         </>
       );
   }
--- src/TodoTable.jsx
+++ src/TodoTable.jsx
@@ -1,26 +1,28 @@
-// import React from "react"; <- tämä ei tee mitään muuta kuin ilmoita virheestä?
+// // import React from "react"; <- tämä ei tee mitään muuta kuin ilmoita virheestä?
 
-function TodoTable(props) {
+// function TodoTable(props) {
     
-      return (
-          <>
-            <table>
-              <tbody>
-                <tr>
-                  <th>Date</th>
-                  <th>Description</th>
-                </tr>
-                {props.todos.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.date}</td>
-                    <td>{item.description}</td>
-                    <td><button onClick={() => props.handleDelete(index)}>Delete</button></td>
-                  </tr>
-                ))}
-              </tbody>
-            </table> 
-          </>
-        );
-  }
+//       return (
+//           <>
+//             <table>
+//               <tbody>
+//                 <tr>
+//                   <th>Date</th>
+//                   <th>Priority</th>
+//                   <th>Description</th>
+//                 </tr>
+//                 {props.todos.map((item, index) => (
+//                   <tr key={index}>
+//                     <td>{item.date}</td>
+//                     <td>{item.priority}</td>
+//                     <td>{item.desc}</td>
+//                     <td><button onClick={() => props.handleDelete(index)}>Delete</button></td>
+//                   </tr>
+//                 ))}
+//               </tbody>
+//             </table> 
+//           </>
+//         );
+//   }
   
-  export default TodoTable;
+//   export default TodoTable;
